1423372de9ae55d89f5c66e1d002dbe534b1334e
frankcasanova <frankcasanova.info@gmail.com>
2021-07-01 13:17:32 +0200

    set postgresql config

--- core/settings.py
+++ core/settings.py
@@ -74,9 +74,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'newsletterDB',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '1234',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '5432',
+
     }
 }
 
