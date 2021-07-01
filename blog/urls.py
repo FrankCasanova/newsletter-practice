e455a9b78b2a7ac266e23036ed071d5370fc936e
frankcasanova <frankcasanova.info@gmail.com>
2021-07-01 18:15:01 +0200

    post detail has been added

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view, name='post_detail'),
     path('', BlogListView.as_view(), name='home')
 ]
