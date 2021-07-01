1ebf0907518651ea42bc5d16c030ca3443c82a63
frankcasanova <frankcasanova.info@gmail.com>
2021-07-01 21:01:19 +0200

    detail view added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -10,3 +10,9 @@ class BlogListView(ListView):
 
     model = Post
     template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):  # this view spect a pk or a slugname cos... is detail
+
+    model = Post
+    template_name = 'post_detail.html'
